b7905224eb57e4bc7e28514ceb30cdc8e4427700
AsterDecember <aster.december@gmail.com>
2018-12-14 11:08:12 -0600

    Books components created, added on routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,18 @@ import Home from './components/home/Home'
 import AuthPage from './components/auth/AuthPage';
 // import BooksList from './components/books/AuthorList';
 // import ProfilePage from './components/authors/ProfilePage';
+import BooksList from './components/books/BooksList'
+import BookDetail from './components/books/BookDetail'
+import BookCreate from './components/books/BookCreate'
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={Home}/>
     <Route path='/signup' component={AuthPage}/>
     <Route path='/login' component={AuthPage}/>
+    <Route path='/books' component = {BooksList} />
+    <Route path='/books/:id' component = {BookDetail} />
+    <Route path='/books/new' component = {BookCreate} />
     {/* <Route path='/profile' component={ProfilePage}/> */}
   </Switch>
 )
