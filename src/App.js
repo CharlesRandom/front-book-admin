44c331d8a64f24b8d2e4da5561c5f48f6466e39d
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-12-14 13:38:37 -0600

    Version 1.0 done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Routes from './Routes';
-import { Link } from 'react-router-dom'
-import {logout} from './services/auth'
 
 class App extends Component {
-
-
   render() {
-    const {onLogout} = this
     return (
       <div className="App">
-
         <Routes/>
       </div>
     );
